29eb67468cab83af9ec0b0ddc23c44c87113d300
Daniel Abdi <daniel.abdi@noaa.gov>
2023-09-25 10:43:14 -0600

    Add initialization option for advection solver.

--- apps/convection/convection.cpp
+++ apps/convection/convection.cpp
@@ -2,49 +2,6 @@
 #include "iteration.h"
 #include "wrapper.h"
 
-//#define OVERWRITE_WIND
-
-/**
-Wind field intialization for Scalar advection
-*/
-#ifdef OVERWRITE_WIND
-void init_wind_field(Scalar time, Scalar etime, VectorCellField& U) {
-    using namespace Mesh;
-    using namespace Constants;
-
-    Scalar radius = 6371220.0;
-    Scalar period = etime;
-    Scalar RoT = radius / period; 
-    forEach(cC,i) {
-        Scalar x = cC[i][0], y = cC[i][1], z = cC[i][2];
-        Vector s = cart_to_sphere(cC[i]);
-        Scalar lat = s[1], lon = s[2]; 
-        Scalar lambda = lon - 2.0 * PI * time / period;
-        Scalar u,v;
-
-        if(Mesh::is_spherical) {
-#if 1
-            u = 10.0 * RoT * pow(sin(lambda),2.0) * sin(2.0 * lat) * 
-                   cos(PI * time / period) + 2.0 * PI * RoT * cos(lat);
-            v = 10.0 * RoT * sin(2.0 * lambda) * cos(lat) * cos(PI * time / period);
-#elif 0
-            u = -5.0 * RoT * pow(sin(0.5 * lambda),2.0) * sin(2.0 * lat) * pow(cos(lat),2.0) *
-                   cos(PI * time / period) + 2.0 * PI * RoT * cos(lat);
-            v = 2.5 * RoT * sin(lambda) * pow(cos(lat),3.0) * cos(PI * time / period);
-#endif
-        } else {
-            u = pow(sin(PI * x), 2.0) * sin(2 * PI * y) * cos(PI * time / period);
-            v = -pow(sin(PI * y), 2.0) * sin(2 * PI * x) * cos(PI * time / period);
-        }
-
-        if(Mesh::is_spherical)
-            U[i] = wind_field(u,v,lat,lon);
-        else
-            U[i] = Vector(u,v,0);
-    }
-}
-#endif
-
 /**
   \verbatim
   Convection solver
@@ -57,10 +14,17 @@ void init_wind_field(Scalar time, Scalar etime, VectorCellField& U) {
 void convection(std::istream& input) {
     /*Solver specific parameters*/
     Scalar t_UR = Scalar(1);
+    enum PROBLEM_INIT {
+        NONE, LEVEQUE, LAURITZEN_0, LAURITZEN_1
+    };
+    PROBLEM_INIT problem_init = NONE;
 
-    /*transport*/
+    /*advection options*/
     Util::ParamList params("convection");
     params.enroll("t_UR", &t_UR);
+    Util::Option* op = new Util::Option(&problem_init,
+            {"NONE", "LEVEQUE", "LAURITZEN_0", "LAURITZEN_1"});
+    params.enroll("problem_init", op);
 
     /*read parameters*/
     Util::read_params(input,MP::printOn);
@@ -70,26 +34,77 @@ void convection(std::istream& input) {
 
         VectorCellField U("U", READWRITE);
         ScalarCellField T("T", READWRITE);
+
+        /**
+        Wind field intialization for Scalar advection
+        */
+        auto init_wind_field = [&] (Scalar time, Scalar etime) {
+            using namespace Mesh;
+            using namespace Constants;
+        
+            Scalar radius = 6371220.0;
+            Scalar period = etime;
+            Scalar RoT = radius / period; 
+
+            #pragma omp parallel for
+            #pragma acc parallel loop
+            forEach(cC,i) {
+                Scalar x = cC[i][0], y = cC[i][1], z = cC[i][2];
+                Scalar u,v;
+        
+                if(Mesh::is_spherical) {
+                    Vector s = cart_to_sphere(cC[i]);
+                    Scalar lat = s[1], lon = s[2]; 
+                    Scalar lambda = lon - 2.0 * PI * time / period;
+
+                    if(problem_init == LAURITZEN_0) {
+                        u = 10.0 * RoT * pow(sin(lambda),2.0) * sin(2.0 * lat) * 
+                               cos(PI * time / period) + 2.0 * PI * RoT * cos(lat);
+                        v = 10.0 * RoT * sin(2.0 * lambda) * cos(lat) * cos(PI * time / period);
+                    } else if (problem_init == LAURITZEN_1) {
+                        u = -5.0 * RoT * pow(sin(0.5 * lambda),2.0) * sin(2.0 * lat) * pow(cos(lat),2.0) *
+                               cos(PI * time / period) + 2.0 * PI * RoT * cos(lat);
+                        v = 2.5 * RoT * sin(lambda) * pow(cos(lat),3.0) * cos(PI * time / period);
+                    }
+
+                    U[i] = wind_field(u,v,lat,lon);
+                } else {
+                    if(problem_init == LEVEQUE) {
+                        u = pow(sin(PI * x), 2.0) * sin(2 * PI * y) * cos(PI * time / period);
+                        v = -pow(sin(PI * y), 2.0) * sin(2 * PI * x) * cos(PI * time / period);
+                    }
+
+                    U[i] = Vector(u,v,0);
+                }
+            }
+        };
+
+        /*special initializations*/
+        Iteration it(ait.get_step());
         VectorCellField Fc;
         ScalarFacetField F;
 
-        /*Time loop*/
-        Iteration it(ait.get_step());
-#ifdef OVERWRITE_WIND
-        Scalar ctime = it.get_step() * Controls::dt;
-        Scalar etime = Controls::end_step * Controls::dt;
-        init_wind_field(ctime, etime, U);
-#endif
-        Fc = flxc(U);
-        F = flx(U);
-        for (; !it.end(); it.next()) {
-#ifdef OVERWRITE_WIND
+        if(ait.get_step() == 0 && problem_init != NONE) {
             Scalar ctime = it.get_step() * Controls::dt;
             Scalar etime = Controls::end_step * Controls::dt;
-            init_wind_field(ctime, etime, U);
-            Fc = flxc(U);
-            F = flx(U);
-#endif
+            init_wind_field(ctime, etime);
+            U.write(0);
+        }
+        Fc = flxc(U);
+        F = flx(U);
+
+        /*Time loop*/
+        for (; !it.end(); it.next()) {
+
+            /*overwrite wind field at all time steps*/
+            if(problem_init != NONE) {
+                Scalar ctime = it.get_step() * Controls::dt;
+                Scalar etime = Controls::end_step * Controls::dt;
+                init_wind_field(ctime, etime);
+                Fc = flxc(U);
+                F = flx(U);
+            }
+
             ScalarCellMatrix M;
             M = convection(T, Fc, F, t_UR);
             Solve(M);
